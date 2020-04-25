cd89dc39faed1522f50edb7153bc64f6237dfb8d
AnastasiaDro <adrogunova@gmail.com>
2020-04-26 00:33:31 +0300

    - crating days data displaying in WeekWeatherFragment

--- app/src/main/java/com/geekbrains/anasdroweather2/ui/home/WeekDataParser.java
+++ app/src/main/java/com/geekbrains/anasdroweather2/ui/home/WeekDataParser.java
@@ -5,58 +5,73 @@ import com.geekbrains.anasdroweather2.model.MyData;
 import java.util.HashMap;
 
 public class WeekDataParser {
-    MyData myData;
-    HashMap <Integer, String[]> allWeatherDataHashMap;
-    String [] zeroArr;
-    int curTime;
-    String curTimeString;
-    int twentyFourHours;
+    private MyData myData;
+    private HashMap<Integer, String[]> allWeatherDataHashMap;
+    private String[] zeroArr;
+    private int curTime;
+    private String curTimeString;
+    private int twentyFourHours;
     //"Шаг" между значением времени в элементах массива
     //У нас он сейчас 3
-    int timeStep;
+    private int timeStep;
 
-    int firstDayBeginIndex;
-
-
+    private int firstDayBeginIndex;
+    private int[] dayBeginningIndexesArr;
+    private int[] morAftEvIndexesArr;
+    private HashMap indexesForFourDaysHashMap;
     //для первого дня
     //это 9 утра, 15 дня, и 21 - вечер
 
 
-
-    public WeekDataParser(){
+    public WeekDataParser() {
         myData = MyData.getInstance();
-        allWeatherDataHashMap = myData.getAllWeatherDataHashMap();
+        indexesForFourDaysHashMap = new HashMap();
         twentyFourHours = 24;
         timeStep = 3;
     }
 
-    public int[] findDayBeginningIndexes(){
+    private void findDayBeginningIndexes() {
+        allWeatherDataHashMap = myData.getAllWeatherDataHashMap();
         //получим время указанное в первом, втором и третьем элементах
         //индексы времени, из которых мы будем получать температуру
-        int [] dayBeginningIndexesArr = new int[4];
+        dayBeginningIndexesArr = new int[3];
         zeroArr = allWeatherDataHashMap.get(0);
         curTimeString = zeroArr[Constants.TIME_KEY_IN_WEATHERDATA_ARRAY];
         curTimeString = curTimeString.substring(0, 2);
         curTime = Integer.parseInt(curTimeString);
-        firstDayBeginIndex = (twentyFourHours - curTime)/3;
+        firstDayBeginIndex = (twentyFourHours - curTime) / 3;
         dayBeginningIndexesArr[0] = firstDayBeginIndex;
-        for (int i = 1; i < 4 ; i++) {
-            dayBeginningIndexesArr[i] = dayBeginningIndexesArr[i-1]+8;
+        for (int i = 1; i < 3; i++) {
+            dayBeginningIndexesArr[i] = dayBeginningIndexesArr[i - 1] + 8;
         }
-        return dayBeginningIndexesArr;
     }
 
     //получим индексы заданного дня для 9.00, 15.00  и 21.00
-    private int [] forDayTempDataIndexes(int dayBeginIndex) {
-        int[] morAftEvIndexesArr = new int[3];
-        morAftEvIndexesArr[0] = dayBeginIndex + 3;
-        morAftEvIndexesArr[1] = dayBeginIndex + 5;
-        morAftEvIndexesArr[2] = dayBeginIndex + 7;
+    private int[] forDayTempDataIndexes(int dayBeginIndex) {
+        morAftEvIndexesArr = new int[3];
+        try {
+            morAftEvIndexesArr[0] = dayBeginIndex + 3;
+            morAftEvIndexesArr[1] = dayBeginIndex + 5;
+            morAftEvIndexesArr[2] = dayBeginIndex + 7;
+        } catch (NullPointerException e) {
+
+        }
         return morAftEvIndexesArr;
     }
+
+    //получим ХэшМап с данными для каждого дня
+    public HashMap takeForEachDayIndexMap() {
+        findDayBeginningIndexes();
+        for (int i = 0; i < dayBeginningIndexesArr.length; i++) {
+            indexesForFourDaysHashMap.put(dayBeginningIndexesArr[i], forDayTempDataIndexes(dayBeginningIndexesArr[i]));
+        }
+        return indexesForFourDaysHashMap;
+    }
+
+}
+
 
     //получим данные по осадкам за все дни и выберем дождь, или экстрим
 
 
 
-}
--- app/src/main/java/com/geekbrains/anasdroweather2/ui/home/WeekWeatherFragment.java
+++ app/src/main/java/com/geekbrains/anasdroweather2/ui/home/WeekWeatherFragment.java
@@ -1,6 +1,7 @@
 package com.geekbrains.anasdroweather2.ui.home;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,31 +18,66 @@ import com.geekbrains.anasdroweather2.interfaces.FragmentMethods;
 import com.geekbrains.anasdroweather2.interfaces.Observer;
 import com.geekbrains.anasdroweather2.model.MyData;
 
+import java.util.HashMap;
+
 public class WeekWeatherFragment extends Fragment implements FragmentMethods, Observer {
 
 
 //TextView по дням недели
 
-    //TextView для названия дней
-    TextView firstDayTextView;
-    TextView scndDayTextView;
-    TextView trdDayTextView;
-    TextView fourthDayTextView;
-    TextView fifthfDayTextView;
-    TextView sixDayTextView;
-    TextView svnDayTextView;
+    //TextView для времени и температур по дням и времени суток
+    //первый день
+    private TextView firstDayTimeText;
+    private TextView fstDayMorTempText;
+    private TextView fstDayAftTempText;
+    private TextView fstDayEvTempText;
+    //второй день
+    private TextView scndDayTimeText;
+    private TextView sndDayMorTempText;
+    private TextView sndDayAftTempText;
+    private TextView sndDayEvTempText;
+    //третий день
+    private TextView thdDayTimeText;
+    private TextView thdDayMorTempText;
+    private TextView thdDayAftTempText;
+    private TextView thdDayEvTempText;
+    //четвертый день
+//    private TextView frthDayTimeText;
+//    private TextView frthDayMorTempText;
+//    private TextView frthAftTempText;
+//    private TextView frthDayEvTempText;
 
-    //TextView для температур по дням
-    TextView firstDayTempText;
-    TextView scndDayTempText;
-    TextView trdDayTempText;
-    TextView fourthDayTempText;
-    TextView fifthDayTempText;
-    TextView sixDayTempText;
-    TextView svnDayTempText;
-    WeekDataParser weekDataParser;
+    //индексы для первого дня
+    int [] firstDayIndexesArr;
+    int fstDayMorKey;
+    int fstDayAftKey;
+    int fstDayEvKey;
+    //массивы для данных первого дня
+    private int []  fstDayMorArr;
+    private int[] fstDayAftArr;
+    private int[] fstDayEvArr;
 
+
+    //индексы для данных второго дня
+    int [] secondDayIndexesArr;
+    int scndDayMorKey;
+    int scndDayAftKey;
+    int scndDayEvKey;
+
+    //индексы для данных третьего дня
+    int [] thirdDayIndexesArr;
+    int thrdDayMorKey;
+    int thrdDayAftKey;
+    int thrdDayEvKey;
+
+
+
+
+
+    private WeekDataParser weekDataParser;
     private MyData myData;
+
+    HashMap dataForTextViewsHashMap = new HashMap();
 
     public static WeekWeatherFragment newInstance() {
         WeekWeatherFragment weekWeatherFragment = new WeekWeatherFragment();
@@ -70,19 +106,69 @@ public class WeekWeatherFragment extends Fragment implements FragmentMethods, Ob
 
     @Override
     public void findViews(View view) {
-        //название дней недели
-        firstDayTextView = view.findViewById(R.id.firstDayTextView);
-        scndDayTextView = view.findViewById(R.id.scndDayTextView);
-        trdDayTextView = view.findViewById(R.id.trdDayTextView);
-        fourthDayTextView = view.findViewById(R.id.fourthDayTextView);
+        //Температура по дням недели: утро, день, вечер
+        //второй день
+        firstDayTimeText = view.findViewById(R.id.firstDayTimeText);
+        fstDayMorTempText = view.findViewById(R.id.firstDayMorningTempText);
+        fstDayAftTempText = view.findViewById(R.id.firstDayAftTempText);
+        fstDayEvTempText = view.findViewById(R.id.firstDayEvTempText);
 
+        //второй день
+        scndDayTimeText = view.findViewById(R.id.scndDayTimeText);
+        sndDayMorTempText = view.findViewById(R.id.scndDayMorningTempText);
+        sndDayAftTempText = view.findViewById(R.id.scndDayAftTempText);
+        sndDayEvTempText = view.findViewById(R.id.scndDayEvTempText);
 
-        //Температура по дням недели
-        firstDayTempText = view.findViewById(R.id.firstDayTempText);
-        scndDayTempText = view.findViewById(R.id.scndDayTempText);
-        trdDayTempText = view.findViewById(R.id.trdDayTempText);
-        fourthDayTempText = view.findViewById(R.id.fourthDayTempText);
+        //третий день
+        thdDayTimeText = view.findViewById(R.id.trdDayTimeText);
+        thdDayMorTempText = view.findViewById(R.id.trdDayMorningTempText);
+        thdDayAftTempText = view.findViewById(R.id.trdDayAftTempText);
+        thdDayEvTempText = view.findViewById(R.id.trdDayEvTempText);
+
+        //четвертый день
+//        frthDayTimeText = view.findViewById(R.id.fourthTimeText);
+//        frthDayMorTempText = view.findViewById(R.id.fourthDayMorningTempText);
+//        frthAftTempText = view.findViewById(R.id.fourthDayAftTempText);
+//        frthDayEvTempText = view.findViewById(R.id.fourthDayEvTempText);
     }
+
+    public void setWeatherValuesToTextViews() {
+        takeAllIndexesForDaysData();
+        final Handler handler = new Handler();
+        //установим данные для первого дня
+
+
+    }
+
+    //получим индексы, по которым будем выбирать из всех выгруженных данных нужные данные по ближайшим дням
+    private void takeAllIndexesForDaysData(){
+        dataForTextViewsHashMap = weekDataParser.takeForEachDayIndexMap();
+        firstDayIndexesArr = (int[]) dataForTextViewsHashMap.get(0);
+        fstDayMorKey = firstDayIndexesArr[0];
+        fstDayAftKey = firstDayIndexesArr[1];
+        fstDayEvKey  = firstDayIndexesArr[2];
+
+        //индексы для данных второго дня
+        secondDayIndexesArr = (int[]) dataForTextViewsHashMap.get(1);
+        scndDayMorKey = secondDayIndexesArr[0];
+        scndDayAftKey = secondDayIndexesArr[1];
+        scndDayEvKey = secondDayIndexesArr[2];
+
+        //индексы для данных третьего дня
+        thirdDayIndexesArr = (int[]) dataForTextViewsHashMap.get(2);
+        thrdDayMorKey = thirdDayIndexesArr[0];
+        thrdDayAftKey = thirdDayIndexesArr[1];
+        thrdDayEvKey = thirdDayIndexesArr[2];
+    }
+
+    //получим массивы строк, по которым будем искать значения
+    private void getStringsArraysWithDaysData(){
+        fstDayMorArr [] = myData.getAllWeatherDataHashMap().get(fstDayMorKey);
+    }
+
+
+
+
 
     @Override
     public void postFragment(AppCompatActivity activity, int placeId) {
