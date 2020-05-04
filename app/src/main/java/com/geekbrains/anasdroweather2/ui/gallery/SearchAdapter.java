c356a3873fb0676d33e2db6ad329547ffa1bbd8b
AnastasiaDro <adrogunova@gmail.com>
2020-05-04 13:45:09 +0300

    - continue create adapter for searchRecycler

--- app/src/main/java/com/geekbrains/anasdroweather2/ui/gallery/SearchAdapter.java
+++ app/src/main/java/com/geekbrains/anasdroweather2/ui/gallery/SearchAdapter.java
@@ -2,9 +2,14 @@ package com.geekbrains.anasdroweather2.ui.gallery;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.geekbrains.anasdroweather2.R;
 
 public class SearchAdapter extends RecyclerView.Adapter {
     @NonNull
@@ -15,9 +20,19 @@ public class SearchAdapter extends RecyclerView.Adapter {
 
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-
+        //Элементы в recyclerView
+        SimpleDraweeView searchDraweeView;
+        TextView searchCityTemp;
+        TextView searchCityName;
+        LinearLayout searchCard;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            searchDraweeView = itemView.findViewById(R.id.searchWeathImg);
+            searchCityTemp = itemView.findViewById(R.id.searchCityTempTV);
+            searchCityName = itemView.findViewById(R.id.searchCityNameTV);
+            searchCard = itemView.findViewById(R.id.mySearchCard);
+
+
         }
     }
 
--- app/src/main/java/com/geekbrains/anasdroweather2/ui/gallery/SearchHistoryFragment.java
+++ app/src/main/java/com/geekbrains/anasdroweather2/ui/gallery/SearchHistoryFragment.java
@@ -1,6 +1,11 @@
 package com.geekbrains.anasdroweather2.ui.gallery;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SearchHistoryFragment extends Fragment {
+    RecyclerView searchRecycler;
+
+
+
 }
